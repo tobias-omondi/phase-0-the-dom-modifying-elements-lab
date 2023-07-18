e8fbb561a10f059f4fe64771a25a060e98ee8bb9
tobias <tobias.omondi@student.moringaschool.com>
2023-07-18 23:23:44 +0300

    my-assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element1=document.getElementById('main');
+    element1.remove('main')
+    const newHeader = document.createElement('h1')
+ newHeader.id="victory"
+newHeader.textContent="YOUR-NAME is the champion"
